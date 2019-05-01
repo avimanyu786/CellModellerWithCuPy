8938165b1a61daa383a11131c4801b988c818f23
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2019-05-01 16:00:55 +0530

    Update StaphylococcusAureusCuPyDoublePrecision.py

--- Experiments/StaphylococcusAureusCuPyDoublePrecision.py
+++ Experiments/StaphylococcusAureusCuPyDoublePrecision.py
@@ -1,5 +1,5 @@
 from CellModeller.Regulation.ModuleRegulator import ModuleRegulator
-#from CellModeller.Biophysics.BacterialModels.CLBacterium import CLBacterium
+# from CellModeller.Biophysics.BacterialModels.CLBacterium import CLBacterium
 #from CellModeller.Biophysics.BacterialModels.CLBacteriumWithCuPy import CLBacteriumWithCuPy
 from CellModeller.Biophysics.BacterialModels.CLBacteriumWithGammaCuPyDoublePrecision import CLBacteriumWithGammaCuPyDoublePrecision
 from CellModeller.GUI import Renderers
@@ -12,8 +12,9 @@ import cupy
 # cell_growr = {0:2.0x4, 1:1.1x4, 2:0.8x4} #growth rates
 # 30-8-18
 
-cell_cols = {0: [1.0, 1.0, 1.0]}
+cell_cols = {0: [1.0, 0.7, 1.0]}
 #cell_lens = {0: 0.8}
+#Staphylococcus Aureus cell sizes range from 0.8 to 1.0 micrometer
 cell_lens = {0: cupy.random.uniform(0.8,1.0)}
 #cell_growr = {0: 12.0}
 cell_growr = {0: 12.0}
@@ -22,9 +23,9 @@ cell_growr = {0: 12.0}
 
 def setup(sim):
     # Set biophysics, signalling, and regulation models
-    biophys = CLBacteriumWithGammaCuPyDoublePrecision(sim, jitter_z=True, rho=1.0039, u=0.02, gammacoeff=0.47, max_planes=1, max_cells=670812)
-    #biophys = CLBacterium(sim, jitter_z=True, gamma=20, max_planes=1, max_cells=100000)
-    #biophys = CLBacteriumWithGammaCuPyDoublePrecision(sim, jitter_z=True, gamma=10, max_planes=5, max_cells=670812)
+    # biophys = CLBacterium(sim, jitter_z=True, gamma=20, max_planes=1, max_cells=100000)
+    biophys = CLBacteriumWithGammaCuPyDoublePrecision(sim, jitter_z=True, rho=1.0039, u=0.02, gammacoeff=0.47, max_planes=1, max_cells=67081)
+    #biophys = CLBacteriumWithGammaCuPyDoublePrecision(sim, jitter_z=True, gamma=10, max_planes=5, max_cells=67081)
     biophys.addPlane((0, 0, 0), (0, 0, 1), 1.0)  # Base plane
     # biophys.addPlane((20, 0, 0), (-1, 0, 0), 1.0)
     # biophys.addPlane((-20, 0, 0), (1, 0, 0), 1.0)
@@ -61,22 +62,36 @@ def setup(sim):
 def init(cell):
     # Specify mean and distribution of initial cell size
     #cellVol = cell_lens[cell.cellType]
-    cellVol= (((cell_lens[cell.cellType]**3)*3.14) / 6)
+    #cellVol= ((cell_lens[cell.cellType]**3)*3.14) / 6
+    cellVol = cupy.divide ( cupy.multiply ( cupy.power ( cell_lens[cell.cellType] , 3 ) , cupy.pi ) , 6 )
 
-    cell.targetVol = cellVol + cupy.random.uniform(0.0,1.5)
+    cell.targetVol = cupy.add ( cellVol , cupy.random.uniform (0.0, 1.5) )
+    #cell.targetVol = cellVol + cupy.random.uniform (0.0, 1.5)
+
     # Specify growth rate and color of cells
-    cell.growthRate = cell_growr[cell.cellType]
+    #cell.growthRate = cell_growr[cell.cellType]
+    cell.growthRate = 1.0
+
+    #Variable growth rates from 1.0 to 12.0
+    #cell.growthRate = cupy.random.uniform(1.0,cell_growr[cell.cellType])
     cell.color = cell_cols[cell.cellType]
 
 
 def update(cells):
     # Iterate through each cell and flag cells that reach target size for division
     for (id, cell) in cells.iteritems():
+        # Variable growth rates from 1.0 to 12.0
+        cell.growthRate = cupy.random.uniform(1.0, cell_growr[cell.cellType])
+
         if cell.volume > cell.targetVol:
             cell.divideFlag = True
 
 
 def divide(parent, d1, d2):
     # Specify target cell size that triggers cell division
-    d1.targetVol = (((cell_lens[parent.cellType]**3)*3.14) / 6) + cupy.random.uniform(0.0,1.5)
-    d2.targetVol = (((cell_lens[parent.cellType]**3)*3.14) / 6) + cupy.random.uniform(0.0,1.5)
+    #d1.targetVol = (((cell_lens[parent.cellType]**3)*3.14) / 6) + cupy.random.uniform(0.0,1.5)
+    #d2.targetVol = (((cell_lens[parent.cellType]**3)*3.14) / 6) + cupy.random.uniform(0.0,1.5)
+
+    d1.targetVol = cupy.add ( cupy.divide ( cupy.multiply ( cupy.power ( cell_lens[parent.cellType] , 3 ) , cupy.pi ) , 6 ) , cupy.random.uniform (0.0 , 1.5) )
+    d2.targetVol = cupy.add ( cupy.divide ( cupy.multiply ( cupy.power ( cell_lens[parent.cellType] , 3 ) , cupy.pi ) , 6 ) , cupy.random.uniform (0.0 , 1.5) )
+
