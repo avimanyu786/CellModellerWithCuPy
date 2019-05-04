658966b0a271ba93c2659d5b536fada190495c6e
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2019-05-04 16:06:13 +0530

    Update moveCellsAsTuplesViaDLPack.py

--- Experiments/moveCellsAsTuplesViaDLPack.py
+++ Experiments/moveCellsAsTuplesViaDLPack.py
@@ -30,3 +30,37 @@
         #self.simulator.cellStates[cid].pos_tensor = [self.cell_centers[i][j] for j in range(3)]
         self.set_cells()
         self.updateCellState(cellState)
+
+        
+    def moveCell2(self, cellState, delta_pos):
+        print "cell idx = %d" % cellState.idx
+        i = cellState.idx
+        cid = cellState.id
+        print "cell center = "
+        print self.cell_centers[i]
+        print "delta_pos"
+        print delta_pos
+        #Converting self.cell_centers[i] (tuple) into a CuPy array
+        pos = cupy.array(tuple(self.cell_centers[i]))
+        #Converting pos (CuPy array) into a DLPack Tensor
+        pos_tensor = cupy.fromDlpack(pos.toDlpack())
+
+        #pos_tensor = pos.toDlpack()
+        #pos_tensor = pos.toDlpack(range(0, 1000, 3))
+
+        #Converting delta_pos (tuple) into a CuPy array
+        delta_pos_array = cupy.array(tuple(delta_pos))
+        #Converting delta_pos_array (CuPy array) into a DLPack Tensor
+        delta_pos_tensor = cupy.fromDlpack(delta_pos_array.toDlpack())
+
+        #delta_pos_tensor = delta_pos_array.toDlpack()
+
+
+        #pos[0:3] += cupy.array(tuple(delta_pos))
+        pos_tensor[0:3] += delta_pos_tensor
+
+        #self.cell_centers[i] = pos
+        self.cell_centers[i] = pos_tensor
+        #self.simulator.cellStates[cid].pos = [self.cell_centers[i][j] for j in range(3)]
+        self.simulator.cellStates[cid].pos_tensor = [self.cell_centers[i][j] for j in range(3)]
+        self.set_cells()
