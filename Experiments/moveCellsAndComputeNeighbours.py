c23c5946a6c630c13483d746ad01e7ee780cef55
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2019-05-04 22:07:42 +0530

    Update moveCellsAndComputeNeighbours.py

--- Experiments/moveCellsAndComputeNeighbours.py
+++ Experiments/moveCellsAndComputeNeighbours.py
@@ -85,3 +85,37 @@
                 self.updateCellNeighbours(self.simulator.idxToId)
             for state in self.simulator.cellStates.values():
                 self.updateCellState(state)
+                
+                
+      def progress_finalise(self):
+        self.frame_no += 1
+        self.progress_initialised = False
+        self.seconds_elapsed = numpy.double(time.time() - self.time_begin)
+        self.minutes_elapsed = (numpy.double(self.seconds_elapsed) / 60.0)
+        self.hours_elapsed = (numpy.double(self.minutes_elapsed) / 60.0)
+        if self.frame_no % 10 == 0:
+            print '% 8i    % 8i cells    % 8i contacts    %f hour(s) or %f minute(s) or %f second(s)' % (
+            self.frame_no, self.n_cells, self.n_cts, self.hours_elapsed, self.minutes_elapsed, self.seconds_elapsed)
+        # pull cells from the device and update simulator
+        if self.simulator:
+            self.get_cells()
+            # TJR: added incremental construction of this dict to same places as idToIdx - not fully tested
+            #idxToId = {idx: id for id, idx in self.simulator.idToIdx.iteritems()}
+
+            #sim_items = list(self.simulator.cellStates.iteritems())
+
+            #iidxToId = list(self.simulator.idxToId)
+
+            #idxToId = {idx: id for id, idx in sim_items}
+
+            #abc = cupy.array(sim_items)
+
+            #idxToId = {idx: id for id, idx in cupy.fromDlpack(cupy.ndarray.toDlpack(sim_item))}
+
+            # TJR: add flag for this cos a bit time consuming
+            if self.computeNeighbours:
+                #self.updateCellNeighbours(self.simulator.abc)
+                self.updateCellNeighbours(self.simulator.idxToId)
+
+            for state in self.simulator.cellStates.values():
+                self.updateCellState(state)              
